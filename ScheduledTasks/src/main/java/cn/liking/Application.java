1d9c856f5298c9a44a30af92265bd5291044df48
liking <543347353@qq.com>
2023-11-13 20:50:14 +0800

    springBoot基本配置

--- ScheduledTasks/src/main/java/cn/liking/Application.java
+++ ScheduledTasks/src/main/java/cn/liking/Application.java
@@ -2,8 +2,10 @@ package cn.liking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling //【1】注解方式启动定时任务
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
