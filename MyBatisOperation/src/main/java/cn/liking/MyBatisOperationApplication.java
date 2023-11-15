5f72d76cbf00c26ba3969bcc03116b58b480af09
liking <543347353@qq.com>
2023-11-15 16:49:46 +0800

    Mybatis基础框架依赖

---
+++ MyBatisOperation/src/main/java/cn/liking/MyBatisOperationApplication.java
@@ -0,0 +1,16 @@
+package cn.liking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * @author LIDADA
+ * Mybatis操作
+ */
+@SpringBootApplication
+public class MyBatisOperationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyBatisOperationApplication.class, args);
+    }
+}
