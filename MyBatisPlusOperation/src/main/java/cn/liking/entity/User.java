f02815b48fa437fe9daefa7d08b60704252f38a8
liking <543347353@qq.com>
2023-11-15 21:30:14 +0800

    MybatisPlus数据库与实体类不匹配处理方法。

--- MyBatisPlusOperation/src/main/java/cn/liking/entity/User.java
+++ MyBatisPlusOperation/src/main/java/cn/liking/entity/User.java
@@ -1,5 +1,7 @@
 package cn.liking.entity;
 
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.*;
 
 //lombok
@@ -8,11 +10,30 @@ import lombok.*;
  * @author liking
  * 用户实体类
  */
+
+
 @Data
+@TableName("user")
 public class User {
     private Long id;
+
     private String name;
+    /**
+     * 采用默认查询开放了更多的字段查看权限
+     */
+    @TableField(select = false)
     private String password;
+
     private Integer age;
-    private String tel;
+    /**
+     * 表字段与编码属性设计不对应时，使用@TableField注解
+     */
+    @TableField("tel")
+    private String telephone;
+    /**
+     * 编码中添加了数据库中未定义的属性
+     */
+    @TableField(exist = false)
+    private String address;
+
 }
