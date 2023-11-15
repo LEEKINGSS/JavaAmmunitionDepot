2944d2f4b22a00ee624db960e09b9b3f5bf1ef90
liking <543347353@qq.com>
2023-11-15 20:42:59 +0800

    MybatisPlus基础CRUD及其分页操作

---
+++ MyBatisPlusOperation/src/main/java/cn/liking/config/MybatisPlusConfig.java
@@ -0,0 +1,26 @@
+package cn.liking.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * MybatisPlus配置类
+ * @author liking
+ */
+@Configuration
+public class MybatisPlusConfig {
+
+    /**
+     * 注册MybatisPlus分页插件[1]
+     */
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        //1 创建MybatisPlusInterceptor拦截器对象
+        MybatisPlusInterceptor mpInterceptor=new MybatisPlusInterceptor();
+        //2 添加分页拦截器
+        mpInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return mpInterceptor;
+    }
+}
--- MyBatisPlusOperation/src/test/java/cn/liking/MyBatisPlusTest.java
+++ MyBatisPlusOperation/src/test/java/cn/liking/MyBatisPlusTest.java
@@ -1,6 +1,9 @@
 package cn.liking;
 
+import cn.liking.entity.User;
 import cn.liking.mapper.UserMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,13 +13,78 @@ import org.springframework.boot.test.context.SpringBootTest;
  * @description 数据库测试类
  * @date: 2023/11/15 19:18
  */
-@SpringBootTest
+@SpringBootTest(classes = MyBatisPlusOperationApplication.class)
 public class MyBatisPlusTest {
     @Autowired
     private UserMapper userMapper;
 
+
+
+    /**
+     * 保存数据
+     */
+    @Test
+    void testSave(){
+        User user = new User();
+        user.setName("liking");
+        user.setPassword("123.com");
+        user.setAge(12);
+        user.setTel("4006184000");
+        userMapper.insert(user);
+    }
+
+    /**
+     * 删除数据
+     */
+    @Test
+    void testDelete(){
+        userMapper.deleteById(1724766644099633154L);
+    }
+
+    /**
+     * 修改数据
+     */
+    @Test
+    void testUpdate(){
+        User user = new User();
+        user.setId(1L);
+        user.setName("liking");
+        user.setPassword("123.com");
+        user.setAge(12);
+        user.setTel("4006184000");
+        userMapper.updateById(user);
+    }
+
+    /**
+     * 查询数据
+     */
+    @Test
+    void testSelect(){
+        System.out.println(userMapper.selectById(1L));
+    }
+
+    /**
+     * 查询所有数据
+     */
     @Test
     void testGetUserAll(){
         System.out.println(userMapper.selectList(null));
+    }
+
+    /**
+     * 分页查询[1]
+     */
+    @Test
+    void testSelectPage(){
+        //1 创建IPage分页对象,设置分页参数
+        IPage<User> page=new Page<>(1,3);
+        //2 执行分页查询
+        userMapper.selectPage(page,null);
+        //3 获取分页结果
+        System.out.println("当前页码值："+page.getCurrent());
+        System.out.println("每页显示数："+page.getSize());
+        System.out.println("总页数："+page.getPages());
+        System.out.println("总条数："+page.getTotal());
+        System.out.println("当前页数据："+page.getRecords());
     }
 }
