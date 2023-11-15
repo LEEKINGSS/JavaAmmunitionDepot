f5a08b89a59f1204061cdebebbc0d281a6d5bc23
liking <543347353@qq.com>
2023-11-15 21:20:21 +0800

    MybatisPlus基础CRUD及其分页操作

--- MyBatisPlusOperation/src/test/java/cn/liking/MyBatisPlusTest.java
+++ MyBatisPlusOperation/src/test/java/cn/liking/MyBatisPlusTest.java
@@ -19,72 +19,39 @@ public class MyBatisPlusTest {
     private UserMapper userMapper;
 
 
-
     /**
-     * 保存数据
+     * mybatis-plus基本操作数据库
      */
     @Test
-    void testSave(){
+    void test(){
+        //保存数据
         User user = new User();
         user.setName("liking");
         user.setPassword("123.com");
         user.setAge(12);
         user.setTel("4006184000");
         userMapper.insert(user);
-    }
-
-    /**
-     * 删除数据
-     */
-    @Test
-    void testDelete(){
-        userMapper.deleteById(1724766644099633154L);
-    }
-
-    /**
-     * 修改数据
-     */
-    @Test
-    void testUpdate(){
-        User user = new User();
+        //查询数据
+        System.out.println(userMapper.selectById(1L));
+        //修改数据
         user.setId(1L);
         user.setName("liking");
         user.setPassword("123.com");
         user.setAge(12);
         user.setTel("4006184000");
         userMapper.updateById(user);
-    }
-
-    /**
-     * 查询数据
-     */
-    @Test
-    void testSelect(){
-        System.out.println(userMapper.selectById(1L));
-    }
-
-    /**
-     * 查询所有数据
-     */
-    @Test
-    void testGetUserAll(){
+        //删除数据
+        userMapper.deleteById(1L);
+        //查询所有数据
         System.out.println(userMapper.selectList(null));
-    }
-
-    /**
-     * 分页查询[1]
-     */
-    @Test
-    void testSelectPage(){
-        //1 创建IPage分页对象,设置分页参数
+        //分页查询
         IPage<User> page=new Page<>(1,3);
-        //2 执行分页查询
         userMapper.selectPage(page,null);
-        //3 获取分页结果
         System.out.println("当前页码值："+page.getCurrent());
         System.out.println("每页显示数："+page.getSize());
         System.out.println("总页数："+page.getPages());
         System.out.println("总条数："+page.getTotal());
         System.out.println("当前页数据："+page.getRecords());
     }
+
 }
