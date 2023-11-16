ff76d7d43a5f9c1c27e3df65351f5fb0a60c9923
liking <543347353@qq.com>
2023-11-16 15:51:38 +0800

    mybatisplus代码自动生成案例

---
+++ MyBatisPlusOperation/src/main/java/cn/liking/service/ICountriesService.java
@@ -0,0 +1,16 @@
+package cn.liking.service;
+
+import cn.liking.entity.Countries;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author liking
+ * @since 2023-11-16
+ */
+public interface ICountriesService extends IService<Countries> {
+
+}
---
+++ MyBatisPlusOperation/src/main/java/cn/liking/service/impl/CountriesServiceImpl.java
@@ -0,0 +1,20 @@
+package cn.liking.service.impl;
+
+import cn.liking.entity.Countries;
+import cn.liking.mapper.CountriesMapper;
+import cn.liking.service.ICountriesService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author liking
+ * @since 2023-11-16
+ */
+@Service
+public class CountriesServiceImpl extends ServiceImpl<CountriesMapper, Countries> implements ICountriesService {
+
+}
