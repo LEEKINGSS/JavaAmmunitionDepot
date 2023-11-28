09e173ccb500ccd7bb81df7bb507341f11f97aae
liking <543347353@qq.com>
2023-11-28 14:33:00 +0800

    SpringBoot项目基础依赖配置

---
+++ RedisOperation/src/main/java/cn/liking/RedisOperationApplication.java
@@ -0,0 +1,16 @@
+package cn.liking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ */
+
+@SpringBootApplication
+public class RedisOperationApplication {
+
+        public static void main(String[] args) {
+                SpringApplication.run(RedisOperationApplication.class, args);
+        }
+}
