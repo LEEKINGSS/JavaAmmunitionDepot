8afa6bd0ec2f4274da59e7007abeac7947f57c84
liking <543347353@qq.com>
2023-11-28 15:10:26 +0800

    SpringBoot项目系统常量

---
+++ RedisOperation/src/main/java/cn/liking/common/SystemConstant.java
@@ -0,0 +1,13 @@
+package cn.liking.common;
+
+/**
+ * 系统常量
+ * @author liking
+ */
+
+public class SystemConstant {
+    /**
+     * websocket连接处理redis中的消息名
+     */
+    public static final String WS_TOPIC = "WS_MSG";
+}
